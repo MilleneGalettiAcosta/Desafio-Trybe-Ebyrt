37421979771ddb20949f4eeff73d510f4ba3f552
Millene Galetti <mikagaletti1@gmail.com>
2022-07-01 17:39:01 -0300

    Feat: Adiciona estrutura de inputs e botao na pagina Task

--- app/frontend/src/pages/Task.tsx
+++ app/frontend/src/pages/Task.tsx
@@ -5,7 +5,13 @@ interface ITaskProps {}
 const Task: React.FC<ITaskProps> = () => {
   return (
     <>
-      
+    <div className="task-container">
+      <section className="task-input">
+        <input type="text" placeholder="Tarefas..." name="task" value="task" onChange={() => {}}/>
+        <input type="number" placeholder="Prazo(em dias)..." name="deadline" value="deadline" onChange={() => {}}/>
+      </section>
+      <button type="button" onClick={() =>{}}>Adicionar tarefa</button>
+      </div>  
     </>
   );
 };
