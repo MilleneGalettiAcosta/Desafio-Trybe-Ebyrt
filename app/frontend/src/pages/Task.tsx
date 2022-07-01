cc75c42481ff5ca05c5585cee2ebd28e7899f401
Millene Galetti <mikagaletti1@gmail.com>
2022-07-01 18:15:35 -0300

    Feat: Adiciona função handleChange para modificar os inputs

--- app/frontend/src/pages/Task.tsx
+++ app/frontend/src/pages/Task.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { ITask } from '../interfaces/Task.interface';
 
 const Task: React.FC = () => {
@@ -7,12 +7,20 @@ const Task: React.FC = () => {
   const [deadline, setDeadline] = useState<number>(0);
   const [tasklist, setTaskList] = useState<ITask[]>([]);
 
+  const handleChange = (event: ChangeEvent<HTMLInputElement>):void => {
+    if (event.target.name === "task") {
+      setTask(event.target.value)
+    } else {
+      setDeadline(Number(event.target.value));
+    }
+  }
+
   return (
     <>
     <div className="task-container">
       <section className="task-input">
-        <input type="text" placeholder="Tarefas..." name="task" value="task" onChange={() => {}}/>
-        <input type="number" placeholder="Prazo(em dias)..." name="deadline" value="0" onChange={() => {}}/>
+        <input type="text" placeholder="Tarefas..." name="task" value={ task } onChange={handleChange}/>
+        <input type="number" placeholder="Prazo(em dias)..." name="deadline" value={ deadline } onChange={handleChange}/>
       </section>
       <button type="button" onClick={() =>{}}>Adicionar tarefa</button>
       </div>  
