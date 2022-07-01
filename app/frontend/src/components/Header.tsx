dd59c80acbfb17d1a132e29ef15f94e8a0b17691
Millene Galetti <mikagaletti1@gmail.com>
2022-07-01 17:22:29 -0300

    Feat: Adiciona estrutura básica do header com seus elementos

--- app/frontend/src/components/Header.tsx
+++ app/frontend/src/components/Header.tsx
@@ -4,9 +4,19 @@ interface IHeaderProps {}
 
 const Header: React.FC<IHeaderProps> = () => {
   return (
-    <>
-      
-    </>
+    <header className="header">
+      <div>
+        <img src="../images/cubo.jpg" alt="cubo verde" />
+      </div>
+      <h1>Lista de Tarefas</h1>
+      <button
+            type="button"
+            onClick={ () => {} }
+          >
+            Sair
+            <img src="../images/exit.png" alt="Sair do aplicativo"/>
+          </button>
+    </header>
   );
 };
 
