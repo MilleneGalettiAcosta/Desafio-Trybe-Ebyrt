9d2a6cc89ad79140e53affb4407f4d6564fd324f
Millene Galetti <mikagaletti1@gmail.com>
2022-07-02 14:15:14 -0300

    Feat: Adiciona props para renderizar nome da página quando componente for utilizado

--- app/frontend/src/components/Header.tsx
+++ app/frontend/src/components/Header.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
-interface IHeaderProps {}
+interface IHeaderProps {
+  page: string;
+}
 
-const Header: React.FC<IHeaderProps> = () => {
+const Header: React.FC<IHeaderProps> = ({ page}: IHeaderProps) => {
   return (
     <header className="header">
       <div>
         <img src="../images/cubo.jpg" alt="cubo verde" />
       </div>
-      <h1>Lista de Tarefas</h1>
+      <h1>{ page }</h1>
       <button
             type="button"
             onClick={ () => {} }
