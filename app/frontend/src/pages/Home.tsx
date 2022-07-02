e83f8a03924f966a06bd24c3cd8049b40e08f117
Millene Galetti <mikagaletti1@gmail.com>
2022-07-02 15:05:26 -0300

    Feat: Adiciona estrutura básica da página Home

--- app/frontend/src/pages/Home.tsx
+++ app/frontend/src/pages/Home.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { cube } from '../images/index';
 
 interface IHomeProps {}
 
-const Home: React.FC<IHomeProps> = ({  }) => {
+const Home: React.FC<IHomeProps> = () => {
   return (
     <>
-      
+      <h1>Ebyrt</h1>
+      <h2>Bem-vindos ao aplcativo de organização de tarefas!</h2>
+      <img src={ cube } alt="logo cubo verde" />
+      <button type="button">Login / Registre-se</button>
     </>
   );
 };
