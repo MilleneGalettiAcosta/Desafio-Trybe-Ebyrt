c7263e52a9395e332b6a80570538e1e9ef4e3be9
Millene Galetti <mikagaletti1@gmail.com>
2022-07-04 15:51:11 -0300

    Feat: Adiciona validação na rota Login Post

--- app/backend/src/routes/LoginRoute.ts
+++ app/backend/src/routes/LoginRoute.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import LoginController from '../controllers/LoginController';
+import LoginMiddleware from '../middlewares/LoginMiddleware';
 
 const router = Router();
 
 // router.get('/');
-router.post('/', LoginController.login);
+router.post('/', LoginMiddleware.validateLogin, LoginController.login);
 
 export default router;
