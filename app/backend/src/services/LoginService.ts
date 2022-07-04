b9bd9d04b0497b55eac540fc0bf4f0e3b5a59432
Millene Galetti <mikagaletti1@gmail.com>
2022-07-04 20:05:15 -0300

    Feat: Adiciona criptografia para password

--- app/backend/src/services/LoginService.ts
+++ app/backend/src/services/LoginService.ts
@@ -1,14 +1,17 @@
 import userModel from '../database/models/LoginModel';
 import ILogin from '../interfaces/Login.interface';
 import ILoginService from './interface/LoginService';
+import crypto from '../auth/Bcrypt';
 
 class LoginService implements ILoginService <ILogin | null> {
   constructor( private models = userModel ) {}
 
-  public login = async (email:string): Promise<ILogin | null> => {
+  public login = async (email:string, password:string): Promise<ILogin | null> => {
     const user = await userModel.findOne({ where: { email } });
 
     if (!user) return null;
+
+    const cryptoPass = crypto(password, user.password)
 
     return {
       email: user.email,
